1c06d5eae942a9e53f1374e6326cc4d7897b3148
Nalin Chopra <nalin.chopra@berkeley.edu>
2018-03-08 17:40:43 -0800

    finished number 2

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171005002456) do
+ActiveRecord::Schema.define(version: 20180309013912) do
 
   create_table "cats", force: :cascade do |t|
     t.string "name"
@@ -23,6 +23,8 @@ ActiveRecord::Schema.define(version: 20171005002456) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "age"
+    t.string "username"
   end
 
 end
